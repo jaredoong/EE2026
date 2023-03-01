542ce87320b186c43351573b3b7fe0b458b2a4f2
jaredoong <jaredoong@gmail.com>
2023-03-01 14:58:50 +0800

    Add default project files

---
+++ MouseAudioDisplay.srcs/sim_1/new/sim20kHzclock.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.03.2023 14:24:13
+// Design Name: 
+// Module Name: sim20kHzclock
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module sim20kHzclock();
+    reg CLOCK;
+    wire SLOW_CLOCK;
+    
+    initial begin
+        CLOCK = 0;
+    end
+    
+    clock_20kHz clk(CLOCK, SLOW_CLOCK);
+    always begin
+        #5 CLOCK = ~CLOCK;
+    end
+endmodule
